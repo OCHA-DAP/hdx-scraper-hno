a9e69acc5bddd3ca535cd21a8d4f7d7db355a2a7
mcarans <rans@email.com>
2025-02-20 11:35:37 +1300

    Remove hard coded feature

--- src/hdx/scraper/hno/__main__.py
+++ src/hdx/scraper/hno/__main__.py
@@ -296,7 +296,6 @@ def main(
 if __name__ == "__main__":
     facade(
         main,
-        hdx_site="feature",
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
         user_agent_lookup=lookup,
         project_config_yaml=script_dir_plus_file(
